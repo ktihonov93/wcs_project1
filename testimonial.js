ae476d39662b3e288bd47b5c5668ce153037a08a
ktihonov93 <k_tihonov@mail.ru>
2021-01-28 17:39:49 +0300

    change testimonial

--- testimonial.js
+++ testimonial.js
@@ -2,14 +2,34 @@ const form = document.querySelector('#close-modal')
 const message = document.querySelector('#message')
 const anotherPersons = document.querySelector('.anotherPersons')
 const stars = document.getElementsByClassName("simple-rating_item")
+const reviewImage = document.querySelector(".reviewImage")
+const reviewStar = document.querySelector(".reviewStar")
+const reviewText = document.querySelector(".reviewText")
+console.log(reviewStar)
 console.log(stars)
 
+//const createStars = () => {
+  for (let i = 0; i < stars.length; i++) {
+    
+    stars[i].addEventListener('click', function () {
+      reviewStar.innerHTML = ''
+      
+      reviewStar.innerHTML = "★".repeat(stars[i].value);
+      console.log(reviewStar)
+      console.log("★".repeat(stars[i].value))
+
+      //createReview()
+    })
+  }
+//}
+//const createReview = () => {
 form.addEventListener('click', function (event) {
 
   // We want to prevent the page to reloas
 
   event.preventDefault();
 
+  //createStars()
 
   // We create a variable that we set to a new li node
 
@@ -21,32 +41,25 @@ form.addEventListener('click', function (event) {
 
   // We add the li to the ul 
 
-  anotherPersons.appendChild(newMessage);
+  reviewText.appendChild(newMessage);
 
   // We clear the value of the input
 
   //inputTodo.value = "";
 
-  const divStars = document.createElement("div");
-    divStars.innerHTML = starImage
+  reviewStar.style.display = "block"
+  reviewStar.style.textAlign = "center";
 
-    anotherPersons.appendChild(divStars);
+  // const divStars = document.createElement("div");
+  //  divStars.innerHTML = starImage
+
+  // anotherPersons.appendChild(divStars);
 
 });
+//}
 
-for (let i = 0; i < stars.length; i++) {
-  stars[i].addEventListener('click', function () {
-
-    console.log(stars[i].value)
-
-    let starImage = "★".repeat(stars[i].value);
-    console.log(starImage)
-
-    // const divStars = document.createElement("div");
-    // divStars.innerHTML = ''
-    // divStars.innerHTML = "★".repeat(stars[i].value);
-
-    // We add the li to the ul 
-  })
+function updateImage() {
+  const file = document.getElementById('user-picture').files[0];
+  document.getElementById('meme-picture').src = window.URL.createObjectURL(file);
 }
 
