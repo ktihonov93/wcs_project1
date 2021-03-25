8c6b90ead98c77e7b1d5ebad9c89dae87144ac7a
Chhavi <Chhavi.choudhary75@gmail.com>
2021-03-25 14:55:26 +0100

    contacts fixed on about page

---
+++ Data.jsx
@@ -0,0 +1,117 @@
+[
+    {
+        "id": 0,
+        "name": "General",
+        "emoji": "🎶",
+        "backgroundC": "#4DC590",
+        "yogaposename1": "Mountain Pose (Tadasana)",
+        "yogaposedescription1": "In mountain pose there is a lot going on, even though it may look like just standing. The heels root down, the muscles of the legs are engaged, the bones are stacked with the shoulders directly over the hips, the shoulder blades slide down the back, and the crown of the head rises. Don't forget to breathe.",
+        "yogaposeimage1": "https://www.verywellfit.com/thmb/oyMr2bOKVVpvR0no1NTGFLP1aJ0=/700x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Verywell-01-3567193-MoutainPose04-099-598b64d49abed50010adff46.jpg",
+        "yogaposename2": "Meditate",
+        "yogaposedescription2": "Take some time to rest, relax, and center your mind to find happiness.",
+        "yogaposeimage2": "https://images.pexels.com/photos/372281/pexels-photo-372281.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "yogaposename3": "Listen to some happy music",
+        "yogaposedescription3": "Putting on music by your favorite artist, or even just searching “happy playlist” on Spotify will be sure to help improve your mood! Trying to focus on the good music can definitely help improve your mind set!",
+        "yogaposeimage3": "https://images.pexels.com/photos/4498195/pexels-photo-4498195.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+    },
+    {
+        "id": 1,
+        "name": "Yoga",
+        "emoji": "🧘",
+        "backgroundC": "#67BBDB",
+        "yogaposename1": "Raised Arms Pose (Urdhva Hastansana)" ,
+        "yogaposedescription1": "Inhale and bring your arms up and over your head. Raised arms pose is your basic morning stretch, but focus on keeping the good alignment you established in mountain pose Stay grounded in the heels and keep your shoulders moving away from your ears at the same time that you reach up through your fingertips. Your gaze can come up to the hands, which can be shoulder's width apart or palms touching."
+        "yogaposeimage1": "https://images.pexels.com/photos/3823207/pexels-photo-3823207.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        "yogaposename2": "Garland Pose (Malasana) ",
+        "yogaposedescription2": " Move your feet out to the edges of your mat and bend your knees, coming into a squat. The toes may turn out if necessary. If your heels do not reach the floor, you can sit on two stacked blocks. This modification makes the pose more accessible for many people.This is a position that is quite natural for children, but we lose the knack for it as adults. It's great for the hips and to counteract the effects of too much sitting in chairs and riding in cars. It's also a very useful pose if you like to garden."
+        "yogaposeimage2": "https://images.pexels.com/photos/4057726/pexels-photo-4057726.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "yogaposename3": "Lunge",
+        "yogaposedescription3": "From malasana, come back into a forward fold with legs gently bent and feet underneath your hips. When you are ready, step your left leg to the back of your mat and bend your right knee for a deep lunge. Try to bring your bent knee directly over your ankle so your right thigh is parallel to the floor. Feel free to place hands on blocks to bring the floor to you.Keep the left leg straight and strong with your heel reaching back. If this is too intense, you can drop your left knee to the mat instead. Stay five breaths before returning the left foot to the front of your mat next to the right one. Then repeat the lunge with the left foot forward and the right leg back.",
+        "yogaposeimage3": "https://images.pexels.com/photos/3820364/pexels-photo-3820364.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        "id": 2,
+        "name": "Physical Exercise",
+        "emoji": "💪",
+        "backgroundC": "#FF7648",
+        "yogaposename1": "Use a stress ball or some other stress reliever",
+        "yogaposedescription1": "The way we handle stress directly affects our health. A common method to deal with stress is a stress ball. Just squeeze the frustrations away!",
+        "yogaposeimage1": "https://images.pexels.com/photos/1579371/pexels-photo-1579371.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+        "yogaposename2": "Pushups",
+        "yogaposedescription2": "PushupsBegin with your chest and stomach flat on the floor,Exhale as you push from your hands and heels, bringing your torso, chest, and thighs off the ground,Pause for a second in the plank position — keep your core engaged.Inhale as you slowly lower back to your starting position.",
+        "yogaposeimage2": "https://images.pexels.com/photos/4775186/pexels-photo-4775186.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "yogaposename3": "Cycling",
+        "yogaposedescription3": "Plan to get on your bike and ride for 30-60 minutes, 3-5 days a week. Start every ride with a warm-up. Pedal at a slow, easy pace for 5-10 minutes. Then boost your speed so you start to sweat.",
+        "yogaposeimage3": "https://images.pexels.com/photos/93776/pexels-photo-93776.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        "id": 3,
+        "name": "Breathing Exercise",
+        "emoji": "👃",
+        "backgroundC": "#8F98FF",
+        "yogaposename1": "Shiitali Kumbhaka or the cooling breath",
+        "yogaposedescription1": "Fold your tongue lengthwise and inhale deeply through the fold. Close your mouth, hold the breath on a count of eight and then exhale through the nose. Continue for a eight breaths, sustain for a maximum of eight minutes.",
+        "yogaposeimage1": "https://images.pexels.com/photos/3059892/pexels-photo-3059892.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "yogaposename2": "Nadi Shodhana or Alternate Nostril Breathing",
+        "yogaposedescription2": "Inhale deeply through your left nostril while holding your right nostril closed with your right thumb. At its culmination, switch nostrils by closing off your left nostril and continuing to exhale smoothly through your right nostril. After exhaling fully, proceed to inhale through the right nostril, again closing it off at the peak of your inhalation. Lift your finger off the left nostril and exhale fully. Continue alternating your breathing through each nostril and practice for 3 to 5 minutes. Ensure that your breathing is effortless, and your mind gently focusing on the inflow and outflow of breath.",
+        "yogaposeimage2": "https://images.pexels.com/photos/4498220/pexels-photo-4498220.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "yogaposename3": "Shavasana (Corpse Pose)",
+        "yogaposedescription3": "Lie down on your back and close your eyes. Relax your body and mind. and think happy peaceful thoughts. Take your time while you do this. Breathe normally and do not hold your breath. After some time, stand up.",
+        "yogaposeimage3": "https://images.pexels.com/photos/3822647/pexels-photo-3822647.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        "id": 4,
+        "name": "Gaming",
+        "emoji": "🎮",
+        "backgroundC": "#F3267C",
+        "yogaposename1": "Play A Board Game",
+        "yogaposedescription1": "Seriously, Monopoly never gets old! Scrabble and Cranium are really fun too.",
+        "yogaposeimage1": "https://images.pexels.com/photos/1634213/pexels-photo-1634213.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "yogaposename2": "Crazy Eight",
+        "yogaposedescription2": "This game is like Uno, but is played with a normal deck of cards. Each player gets seven cards, and the rest of the deck is put in the middle of the playing surface, placing one card face up next to the deck. Players go in a circle, adding cards to the pile that match the suit or rank of the card facing up. If a player cannot match the face-up card, they must choose cards from the face-down pile until they find a matching card. Eights are like wild cards and can be any suit, rank, or color desired. The first player to get rid of all of their cards wins.",
+        "yogaposeimage2": "https://images.pexels.com/photos/39018/cards-jass-cards-card-game-strategy-39018.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "yogaposename3": "Heads-Up, Seven-Up",
+        "yogaposedescription3": "This game will definitely bring you back to elementary school. Players put their heads down and keep their thumbs up. Seven “choosers” go around the room and press one players thumb down. When ready, the “choosers” say, “heads up seven up” and players guess who pressed their thumb down. If correct, the player takes the “choosers” spot.",
+        "yogaposeimage3": "https://images.pexels.com/photos/4547195/pexels-photo-4547195.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    }
+  ]
+
+[
+    {
+        "id": 0,
+        "name": "Dr. Vladyslav",
+        "image": "https://cdn.sanity.io/images/0vv8moc6/hcplive/0ebb6a8f0c2850697532805d09d4ff10e838a74b-200x200.jpg",
+        "title": "Psychologist",
+        "rating": 4.8,
+        "description": "super good doctor super good doctor super good doctor super good doctor.",
+        "qualification": "2 years as a doctor"
+    },
+    {
+        "id": 1,
+        "name": "Dr. Pedro Moczulski",
+        "image": "https://i.imgur.com/fKJhjM0.png",
+        "title": "Psychiatrist",
+        "rating": 4.1,
+        "description": "He was general manager of the hospice in Rio de Janeiro, now lives in Lisbon and helps for all kinds of crazy people!",
+        "qualification": "2 years as a doctor"
+    },
+
+  {
+    "id": 2,
+    "name": "Dr. Konstantin Tihonov",
+    "image": "https://ibb.co/tzhsPzw",
+    "title": "Psychologist",
+    "rating": 4.8,
+    "description": " Doctor Konstantin Tihonov is a pediatric neuropsychologist in private practice at Beljan Psychological Services in Scottsdale AZ. The Arizona Psychological Association selected Dr. Beljan in 2016 for the Distinguished Contribution to the Practice of Psychology Award. Dr. Beljan serves at the Governor's pleasure on the Arizona Board of Psychological Examiners. He completed two Post-Doctoral Fellowship programs specializing in Pediatric (Medical) Psychology and Pediatric Neuropsychology. ",
+    "qualification": "8 years as a doctor"
+    },
+  {
+    "id": 3,
+    "name": "Dr. Chhavi Choudhary",
+    "image": "https://imgur.com/a/GOCQfCl",
+    "title": "Psychiatrist",
+    "rating": 4.1,
+    "description": "She completed her master degree in psychology from Uttarakhand University of psychology,India.She has been employed in the profession ever since. She worked for more than two years in a psychiatric clinic.Her main specialist areas are psychodiagnostics and psychotherapy, combining elements of analytically oriented, exploratory therapy and behavioral therapy.",
+    "qualification": "2 years as a doctor"
+},
+]
